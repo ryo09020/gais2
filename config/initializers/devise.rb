cce56bad31b4d2596dcb7784109ee88045bd7c2a
太田亮 <otaryo@RyoMac-Pro.local>
2024-08-20 13:37:30 +0900

    add some in devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -17,7 +17,8 @@ Devise.setup do |config|
   # config.secret_key = '5d42e4400c88e3d225e3393d3001c9b3dca1cd6c451ca16b1918eeae6e47032eb9f806a4747756f84d3c81597ac70fc8a910b60d312b8f683d0c792328a63a92'
   config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
     scope: 'userinfo.email,userinfo.profile',
-    prompt: 'select_account'
+    prompt: 'select_account',
+    redirect_uri: Rails.env.production? ? 'https://your-production-app.com/users/auth/google_oauth2/callback' : 'http://localhost:3000/users/auth/google_oauth2/callback'
   }
   #↑この記述をconfig/omniauth.rbにも記述していたため、エラーが発生していた。！！！！
 
